e0f6876bb2c33ae8c0846ab4345d9fd1a8e4f539
Rathijit Paul <alienpapon009@gmail.com>
2020-11-13 19:42:07 +0600

    Part 1 Done

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -2,6 +2,10 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import javax.crypto.Mac;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
  * until a certain time.
@@ -11,43 +15,92 @@ public class Alarm {
      * Allocate a new Alarm. Set the machine's timer interrupt handler to this
      * alarm's callback.
      *
-     * <p><b>Note</b>: Nachos will not function correctly with more than one
-     * alarm.
+     * <p>
+     * <b>Note</b>: Nachos will not function correctly with more than one alarm.
      */
     public Alarm() {
-	Machine.timer().setInterruptHandler(new Runnable() {
-		public void run() { timerInterrupt(); }
-	    });
+        Machine.timer().setInterruptHandler(new Runnable() {
+            public void run() {
+                timerInterrupt();
+            }
+        });
     }
 
     /**
      * The timer interrupt handler. This is called by the machine's timer
-     * periodically (approximately every 500 clock ticks). Causes the current
-     * thread to yield, forcing a context switch if there is another thread
-     * that should be run.
+     * periodically (approximately every 500 clock ticks). Causes the current thread
+     * to yield, forcing a context switch if there is another thread that should be
+     * run.
      */
     public void timerInterrupt() {
-	KThread.currentThread().yield();
+
+        boolean initStatus = Machine.interrupt().disable();
+
+        while (!waitQueue.isEmpty()) {
+            PriorThread priorThread = waitQueue.peek();
+            if (priorThread.priority <= Machine.timer().getTime()) {
+                waitQueue.poll();
+                priorThread.thread.ready();
+            } else
+                break;
+
+        }
+
+        Machine.interrupt().restore(initStatus);
+        KThread.currentThread().yield();
+
     }
 
     /**
-     * Put the current thread to sleep for at least <i>x</i> ticks,
-     * waking it up in the timer interrupt handler. The thread must be
-     * woken up (placed in the scheduler ready set) during the first timer
-     * interrupt where
+     * Put the current thread to sleep for at least <i>x</i> ticks, waking it up in
+     * the timer interrupt handler. The thread must be woken up (placed in the
+     * scheduler ready set) during the first timer interrupt where
      *
-     * <p><blockquote>
-     * (current time) >= (WaitUntil called time)+(x)
-     * </blockquote>
+     * <p>
+     * <blockquote> (current time) >= (WaitUntil called time)+(x) </blockquote>
      *
-     * @param	x	the minimum number of clock ticks to wait.
+     * @param x the minimum number of clock ticks to wait.
      *
-     * @see	nachos.machine.Timer#getTime()
+     * @see nachos.machine.Timer#getTime()
      */
     public void waitUntil(long x) {
-	// for now, cheat just to get something working (busy waiting is bad)
-	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
+        // for now, cheat just to get something working (busy waiting is bad)
+        // long wakeTime = Machine.timer().getTime() + x;
+        // while (wakeTime > Machine.timer().getTime())
+        // KThread.yield();
+
+        boolean currentStatus = Machine.interrupt().disable();
+
+        long wakeTime = Machine.timer().getTime() + x;
+
+        KThread currentThread = KThread.currentThread();
+        PriorThread priorThread = new PriorThread(currentThread, wakeTime);
+        waitQueue.add(priorThread);
+        currentThread.sleep();
+
+        Machine.interrupt().restore(currentStatus);
+    }
+
+    private PriorityQueue<PriorThread> waitQueue = new PriorityQueue<>(new PriorThread.Comp());
+
+}
+
+class PriorThread {
+    public KThread thread;
+    public long priority;
+
+    public PriorThread(KThread t, long p) {
+        thread = t;
+        priority = p;
+    }
+
+    public static class Comp implements Comparator<PriorThread> {
+        public int compare(PriorThread a, PriorThread b) {
+            if (a.priority > b.priority)
+                return 1;
+            else if (a.priority < b.priority)
+                return -1;
+            return 0;
+        }
     }
 }
---
+++ nachos/threads/Test.java
@@ -0,0 +1,212 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nachos.threads;
+
+import nachos.machine.*;
+
+/**
+ *
+ * @author ana
+ */
+public class Test {
+
+    public static void initiateTest() {
+        JoinTest.performTest();
+        Condition2Test.performTest();
+        AlarmTest.performTest();
+    }
+
+}
+
+class JoinTest {
+    public JoinTest() {
+
+    }
+
+    public static void performTest() {
+
+        System.out.println();
+        System.out.println("--------------------------------");
+        System.out.println("testing for task 1 initiated");
+        System.out.println("--------------------------------");
+
+        KThread t1 = new KThread(new PingTest(1)).setName("forked thread 1");
+        t1.fork();
+        KThread t2 = new KThread(new PingTest(2)).setName("forked thread 2");
+        t2.fork();
+
+        t1.join();
+
+        KThread t3 = new KThread(new PingTest(3)).setName("forked thread 3");
+        t3.fork();
+
+        KThread t4 = new KThread(new PingTest(4)).setName("forked thread 4");
+        t4.fork();
+
+        t3.join();
+        t2.join();
+        t4.join();
+
+        System.out.println("---------------------------------------------");
+        System.out.println("testing for task 1 finished");
+        System.out.println("---------------------------------------------");
+
+    }
+
+    private static class PingTest implements Runnable {
+
+        PingTest(int which) {
+            this.which = which;
+        }
+
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("*** thread " + which + " looped " + i + " times");
+                KThread.currentThread().yield();
+            }
+        }
+
+        private final int which;
+
+    }
+}
+
+class Condition2Test {
+
+    private static Communicator communicator = new Communicator();
+
+    public Condition2Test() {
+        communicator = new Communicator();
+    }
+
+    public static void performTest() {
+        System.out.println("testing for task 2 & 4 initiated");
+        System.out.println("-----------------------------------");
+
+        KThread l1 = new KThread(new Listener(1, communicator)).setName("listener thread 1");
+        KThread l2 = new KThread(new Listener(2, communicator)).setName("listener thread 2");
+        KThread l3 = new KThread(new Listener(2, communicator)).setName("listener thread 2");
+
+        KThread s1 = new KThread(new Speaker(1, communicator)).setName("speaker thread 1");
+        KThread s2 = new KThread(new Speaker(2, communicator)).setName("speaker thread 2");
+        KThread s3 = new KThread(new Speaker(3, communicator)).setName("speaker thread 3");
+
+        l1.fork();
+        l2.fork();
+        l3.fork();
+
+        s1.fork();
+        s2.fork();
+        s3.fork();
+
+        l1.join();
+        l2.join();
+        l3.join();
+
+        s1.join();
+        s2.join();
+        s3.join();
+
+        System.out.println("---------------------------------------------");
+        System.out.println("testing for task 2 & 4 finished");
+        System.out.println("---------------------------------------------");
+
+    }
+
+    private static class Listener implements Runnable {
+        Listener(int which, Communicator communicator) {
+
+            this.which = which;
+            this.communicator = communicator;
+        }
+
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                KThread.yield();
+                int listenedValue = communicator.listen();
+                System.out.println(KThread.currentThread().getName() + " listened " + listenedValue);
+                KThread.yield();
+            }
+        }
+
+        private int which;
+        private Communicator communicator;
+    }
+
+    private static class Speaker implements Runnable {
+        Speaker(int which, Communicator communicator) {
+
+            this.which = which;
+            this.communicator = communicator;
+        }
+
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                KThread.yield();
+                System.out.println(KThread.currentThread().getName() + " spoke " + i);
+                communicator.speak(i);
+                KThread.yield();
+            }
+        }
+
+        private int which;
+        private Communicator communicator;
+    }
+}
+
+class AlarmTest {
+
+    private static class AlarmTestRunnable implements Runnable {
+
+        AlarmTestRunnable(long time, Alarm alarm) {
+            this.time = time;
+            this.alarm = alarm;
+        }
+
+        public void run() {
+            System.out.println(KThread.currentThread().getName() + " rings at " + Machine.timer().getTime());
+            alarm.waitUntil(time);
+            System.out.println(KThread.currentThread().getName() + " rings at " + Machine.timer().getTime());
+        }
+
+        private final long time;
+        private final Alarm alarm;
+
+    }
+
+    public static void performTest() {
+        System.out.println("testing for task 3 initiated");
+        System.out.println("--------------------------------");
+
+        long time1 = 800000;
+        long time2 = 900000;
+        long time3 = 500000;
+
+        Alarm alarm = new Alarm();
+
+        // create threads for each a test object
+        KThread t1 = new KThread(new AlarmTestRunnable(time1, alarm)).setName("Alarm thread 1");
+        KThread t2 = new KThread(new AlarmTestRunnable(time2, alarm)).setName("Alarm thread 2");
+        KThread t3 = new KThread(new AlarmTestRunnable(time3, alarm)).setName("Alarm thread 3");
+
+        // run threads with alarms
+        t1.fork();
+        t2.fork();
+        t3.fork();
+
+        KThread.yield();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("---------------------------------------------");
+        System.out.println("testing for task 3 finished");
+        System.out.println("---------------------------------------------");
+
+    }
+}
